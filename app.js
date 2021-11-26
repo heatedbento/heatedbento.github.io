7cfe87366ffb57686cdb2396309b6ea071d82493
Denny Sesay <93602674+heatedbento@users.noreply.github.com>
2021-11-26 06:40:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2lrJnCCWjm5vOVIFwtDfyqWrGGZfEqJw",
+  authDomain: "twitchonline.firebaseapp.com",
+  projectId: "twitchonline",
+  storageBucket: "twitchonline.appspot.com",
+  messagingSenderId: "129614631927",
+  appId: "1:129614631927:web:1291550f08b4b78dcc54f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log('hey');
